94722d623695fa4c1fa0a7be9b9e06cee42222eb
dinhnhat0401 <nhatphutho@gmail.com>
2019-10-20 18:42:51 +0900

    swift csv prototype

---
+++ swiftcsv/Pods/Target Support Files/Pods-swiftcsvTests/Pods-swiftcsvTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swiftcsvTests : NSObject
+@end
+@implementation PodsDummy_Pods_swiftcsvTests
+@end
--- swiftcsv/swiftcsv/AppDelegate.swift
+++ swiftcsv/swiftcsv/AppDelegate.swift
@@ -7,14 +7,14 @@
 //
 
 import UIKit
+import SwiftCSV
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        loadCSV()
         return true
     }
 
@@ -32,6 +32,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func loadCSV() {
+        do {
+            let filename = "Book-Authors"
+            let type = "csv"
+            guard let path = Bundle.main.path(forResource: filename, ofType: type) else {
+                print("CANNOT LOAD FILE: " + filename)
+                return
+            }
 
+            let csv = try CSV(url: URL(fileURLWithPath: path))
+            try csv.enumerateAsDict { dict in
+                print(dict["authorID"]!)
+                print(dict["name"]!)
+                print(dict["intro"]!)
+            }
+        } catch let error as NSError {
+            print(error)
+        }
+    }
 }
-
